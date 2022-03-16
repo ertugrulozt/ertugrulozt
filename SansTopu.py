43db289554fb818b0b48ca3c2c6780d23a7ed515
ertugrulozt <ertugrulozt@gmail.com>
2022-03-16 17:37:13 +0300

    16.03.2022- first exe_Sans Topu

---
+++ SansTopu.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+import random
+
+
+window = Tk()
+window.geometry('800x600')
+var= StringVar()
+tb = Label(textvariable=var)
+def sans():
+    kazanan_numaralar = []
+    artiBir = random.sample(range(1, 15), 1)
+
+    while True:
+
+        sayi = random.randint(1, 34)
+
+        if sayi not in kazanan_numaralar:
+            kazanan_numaralar.append(sayi)
+        if len(kazanan_numaralar) == 5:
+            break
+
+    print(f'Numaralar: {sorted(kazanan_numaralar)}\nArtıBir: {artiBir}')
+    var.set(f'Numaralar: {sorted(kazanan_numaralar)}\nArtıBir: {artiBir}')
+
+button = Button(text= 'Generate',command=sans)
+button.place(relx=0.3,rely=0.6,relwidth=0.4,relheight=0.1)
+
+tb.place(relx=0.3,rely=0.3, relwidth=0.4, relheight=0.1)
+window.mainloop()
+
+
+
+
+
